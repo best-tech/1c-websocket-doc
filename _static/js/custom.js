ec2da39a580c0cfa525ca00531b113598c4b104f
Denis Kharchenko <kharchenkodenis@yandex.ru>
2018-11-22 11:25:29 +0300

    temp

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -1,1 +1,24 @@
-alert("dede");
+
+//var temp = document.createElement('div');
+//temp.id = "vk_comments";
+//VK.Widgets.Comments("vk_comments", {limit: 20, attach: false});
+//temp.innerHTML = "dewdede";
+
+
+// function fireOnReady() {
+
+//     var footer = document.getElementsByTagName("footer")[0];
+
+//     var newElement = '<div id="vk_comments"></div><script type="text/javascript" src="https://vk.com/js/api/openapi.js?159"></script>';
+
+//     footer.innerHTML = newElement+footer.innerHTML;
+
+
+
+// }
+
+// if (document.readyState === 'complete') {
+//     fireOnReady();
+// } else {
+//     document.addEventListener("DOMContentLoaded", fireOnReady);
+// }
